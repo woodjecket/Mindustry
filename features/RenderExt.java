b568555527fb48bb05cce1ceba0e66e722ef178f
way-zer <himc.wicp@gmail.com>
2024-02-17 13:56:18 +0800

    C(RenderExt) arcChoiceUiIcon(显示选中物品图标)

--- features/RenderExt.java
+++ features/RenderExt.java
@@ -11,12 +11,14 @@ import mindustry.world.blocks.logic.MessageBlock.*;
 
 public class RenderExt{
     public static boolean bulletShow, showMineBeam, displayAllMessage;
+    public static boolean arcChoiceUiIcon;
 
     public static void init(){
         Events.run(Trigger.preDraw, () -> {
             bulletShow = Core.settings.getBool("bulletShow");
             showMineBeam = Core.settings.getBool("showminebeam");
             displayAllMessage = Core.settings.getBool("displayallmessage");
+            arcChoiceUiIcon = Core.settings.getBool("arcchoiceuiIcon");
         });
         Events.run(Trigger.draw, RenderExt::draw);
     }
