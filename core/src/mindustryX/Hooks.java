30e25cad652323a70ccf960c2988226d41dd3b50
way-zer <himc.wicp@gmail.com>
2024-04-09 18:23:19 +0800

    API(Hooks) onHttp

--- core/src/mindustryX/Hooks.java
+++ core/src/mindustryX/Hooks.java
@@ -4,6 +4,7 @@ import arc.*;
 import arc.files.*;
 import arc.util.*;
 
+import java.net.*;
 import java.util.*;
 
 public class Hooks implements ApplicationListener{
@@ -17,6 +18,22 @@ public class Hooks implements ApplicationListener{
     @Override
     public void init(){
         Log.infoTag("MindustryX", "Hooks.init");
+    }
+
+    @SuppressWarnings("unused")//call before arc.util.Http$HttpRequest.block
+    public static void onHttp(Http.HttpRequest req){
+        if(Core.settings.getBool("githubMirror")){
+            try{
+                String url = req.url;
+                String host = new URL(url).getHost();
+                if(host.contains("github.com") || host.contains("raw.githubusercontent.com")){
+                    url = "https://gh.tinylake.tech/" + url;
+                    req.url = url;
+                }
+            }catch(Exception e){
+                //ignore
+            }
+        }
     }
 
     private static void registerBundle(){
