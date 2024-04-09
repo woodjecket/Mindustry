434fc51ed40eb26c0e9c8b0fb02a73f68f4e6a0b
way-zer <himc.wicp@gmail.com>
2024-04-09 21:23:47 +0800

    sync

--- scripts/applyPatches.sh
+++ scripts/applyPatches.sh
@@ -2,4 +2,6 @@
 
 cd work/ || exit 1
 git tag -f base
-git am --no-gpg-sign ../patches/*
+git am --no-gpg-sign ../patches/picked/*
+git tag -f picked
+git am --no-gpg-sign ../patches/client/*
--- scripts/genPatches.sh
+++ scripts/genPatches.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd work/ || exit 1
-rm ../patches/*
-git format-patch --full-index --no-signature --zero-commit -N -o ../patches base --
+rm ../patches/*/*
+git format-patch --full-index --no-signature --zero-commit -N -o ../patches/picked base...picked
+git format-patch --full-index --no-signature --zero-commit -N -o ../patches/client picked
 cd ../ && git add patches/
