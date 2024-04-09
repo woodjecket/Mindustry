7dab2a43754b7fa9156cb2fa11af24a5b8412fe4
way-zer <himc.wicp@gmail.com>
2024-04-09 18:21:28 +0800

    CS: Bundle Extend

--- Hooks.java
+++ Hooks.java
@@ -1,17 +1,39 @@
 package mindustryX;
 
 import arc.*;
+import arc.files.*;
 import arc.util.*;
+
+import java.util.*;
 
 public class Hooks implements ApplicationListener{
     /** invoke before `Vars.init`. Note that may be executed from `Vars.loadAsync` */
     public static void beforeInit(){
         Log.infoTag("MindustryX", "Hooks.beforeInit");
+        registerBundle();
     }
 
     /** invoke after loading, just before `Mod::init` */
     @Override
     public void init(){
         Log.infoTag("MindustryX", "Hooks.init");
+    }
+
+    private static void registerBundle(){
+        //MDTX: bundle overwrite
+        try{
+            I18NBundle originBundle = Core.bundle;
+            Fi handle = Core.files.internal("bundles/bundle-mdtx");
+            Core.bundle = I18NBundle.createBundle(handle, Locale.getDefault());
+            Reflect.set(Core.bundle, "locale", originBundle.getLocale());
+            Log.info("MDTX: bundle has been loaded.");
+            var rootBundle = Core.bundle;
+            while(rootBundle.getParent() != null){
+                rootBundle = rootBundle.getParent();
+            }
+            Reflect.set(rootBundle, "parent", originBundle);
+        }catch(Throwable e){
+            Log.err(e);
+        }
     }
 }
