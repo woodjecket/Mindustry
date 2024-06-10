4d9e8592def972b616a5fbc5be16160e167770de
way-zer <himc.wicp@gmail.com>
2024-06-10 22:05:21 +0800

    update genPatches.sh

--- scripts/genPatches.sh
+++ scripts/genPatches.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd work/ || exit 1
-rm ../patches/*/*
+(cd ..&&rm -rf patches/*)
 git format-patch --full-index --no-signature --zero-commit -N -o ../patches/picked base...picked
 git format-patch --full-index --no-signature --zero-commit -N -o ../patches/client picked
-cd ../ && git add patches/
+(cd .. && git add patches)
