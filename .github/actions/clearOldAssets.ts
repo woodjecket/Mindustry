d428459f8c6defc4267d3c844a18734d5314dfff
way-zer <himc.wicp@gmail.com>
2024-09-17 18:28:31 +0800

    fix ci

--- .github/actions/clearOldAssets.ts
+++ .github/actions/clearOldAssets.ts
@@ -1,12 +1,17 @@
 import * as core from "@actions/core"
-import {context, getOctokit} from "@actions/github"
+import {getOctokit} from "@actions/github"
 
-const octokit = getOctokit(import.meta.env.GITHUB_TOKEN) as import("@octokit/plugin-rest-endpoint-methods/dist-types/types").Api
-const selfBuild = import.meta.env.RELEASE_VERSION
+const octokit = getOctokit(import.meta.env.TOKEN!) as import("@octokit/plugin-rest-endpoint-methods/dist-types/types").Api
 
-async function clearOldAssets(tag: string, keep: number = 9) {
+const [owner, repo] = import.meta.env.REPOSITORY!.split("/")
+const tag = core.getInput("release") || import.meta.env.TAG!
+const selfBuild = import.meta.env.RELEASE_VERSION!
+console.log(owner, repo, tag, selfBuild)
+
+
+async function clearOldAssets(keep: number = 9) {
     const release = await octokit.rest.repos.getReleaseByTag({
-        ...context.repo, tag
+        owner, repo, tag
     })
     const assets = release.data.assets.sort((a, b) => a.name < b.name ? 1 : -1)
     if (assets.length <= keep) return
@@ -15,9 +20,9 @@ async function clearOldAssets(tag: string, keep: number = 9) {
     console.log("ToRemove", toRemove.map(it => it.name))
     await Promise.all(toRemove.map(it =>
         octokit.rest.repos.deleteReleaseAsset({
-            ...context.repo, asset_id: it.id
+            owner, repo, asset_id: it.id
         })
     ))
 }
 
-await clearOldAssets(core.getInput("release") || import.meta.env.TAG)
+await clearOldAssets()
